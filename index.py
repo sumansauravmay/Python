cb731959ed6187397a011f5240c198a31a4f6498
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-25 17:32:00 +0530

    function

--- index.py
+++ index.py
@@ -1,5 +1,36 @@
-name="suman";
-print(name)
+# name="suman";
+# print(name)
+
+# def hello():
+#     print("Hello")
+# hello()
+
+
+# def name(a):
+#     name=input("Enter city: ")
+#     print("Hello", a,"You are from", name)
+# name("suman")
+ 
+def blank():
+    print("hi")
+
+def blank_3():
+    blank()
+    blank()
+    blank()
+
+def blank_9():
+    blank_3()
+    blank_3()
+    blank_3()
+blank_9()
+# it provides blank space
+
+
+
+
+
+
 
 
 
