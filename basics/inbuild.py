8e23fa3eda8e6ac3e61ae8140c82e3f1de6640fb
sumansauravmay <sumansauravmay12345@gmail.com>
2024-09-04 13:55:29 +0530

    py inbuild

---
+++ basics/inbuild.py
@@ -0,0 +1,21 @@
+print(5//3); #math.floor
+
+
+# the conversion of one data type into the other data type is known as typecasting;
+# we can use some method int(), 
+a="5";
+b="6";
+print(a+b);
+# explit typecasting
+print(int(a)+int(b));
+
+a=5;
+b=6;
+print(a+b);
+
+# implicit typecasting
+
+
+
+
+
