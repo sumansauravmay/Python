2ccfac42cf07ab6da041c318ef633c31d9e72a5f
sumansauravmay <sumansauravmay12345@gmail.com>
2024-09-04 23:32:54 +0530

    loop

---
+++ basics/loop.py
@@ -0,0 +1,72 @@
+# For Loop
+
+# name="suman";
+# for i in name:
+#     print(i)
+
+
+# colors=["Green", "Blue", "Red"];
+# for color in colors:
+#     print(color);
+#     for i in color:
+#         print(i);
+
+
+# for k in range(5):
+#     print(k);
+    
+    
+# for k in range(1,5):
+#     print(k);
+
+# for k in range(1,10,3):
+#     print(k);
+
+# -----------------------------------------------------------------
+# While Loop
+
+
+# count=5;
+# while(count>0):
+#     print(count);
+#     count=count-1;
+
+
+# count=5;
+# while(count>0):
+#     print(count);
+#     count=count-1;
+# else:
+#     print("End the loop!")
+
+# ---------------------------------------------------------------
+
+# Break & Continue
+
+# for i in range(12):
+#     if(i==10):
+#         break;
+#     print(i);
+
+# print("End");
+
+
+# for i in range(12):
+#     if(i==10):
+#         continue;
+#     print(i);
+
+# print("End");
+
+
+
+
+
+
+
+
+
+
+
+
+
