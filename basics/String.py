8218064d9fbaf557d73bc09c937947135653e4e7
sumansauravmay <sumansauravmay12345@gmail.com>
2024-09-04 15:08:28 +0530

    string

--- basics/String.py
+++ basics/String.py
@@ -21,12 +21,30 @@ length=len(name);
 #     print(character);
     
     
-print(name[0:3], length);    
-print(name[0:length]);
-print(name[1:length]);
-print(name[:length]);
+# print(name[0:3], length);    
+# print(name[0:length]);
+# print(name[1:length]);
+# print(name[:length]);
+# print(name[0:length-3]);
+# print(name[0:-3]);
+# print(name[-4:-2]);
 
-print(name[0:length-3]);
-print(name[0:-3]);
-print(name[-4:-2]);
+# print(name.upper());
+# print(name.lower());
+
+
+# print(name.replace("suman", "Akash"))
+# print(name.replace("Suman", "Akash"))
+# print(name.replace(name, "Akash"))
+
+
+
+# name2="!!!!!Suman!!!!!!!!";
+# print(name2.rstrip("!"));
+
+
+x="I am Suman Saurav";
+
+print(x.split(" "));
+print(x.lower().count("s"))
 
