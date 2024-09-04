a94a88a726bfbbc5fb6f8ed2e0a8ccef099b5509
sumansauravmay <sumansauravmay12345@gmail.com>
2024-09-04 14:09:08 +0530

    input

---
+++ basics/input.py
@@ -0,0 +1,15 @@
+# a=input();
+# print("My name is ", a);
+
+# b=input("Enter your name: ");
+# print("My name is ", b);
+
+x=input("Enter first number: ");
+y=input("Enter second number: ");
+
+
+print(x+y);
+print(int(x)+int(y));
+
+
+
