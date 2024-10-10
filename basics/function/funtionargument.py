5ec9a4ec9f067bb6e7a5f9f8a5a96377b953575d
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-11 00:01:52 +0530

    function arguments

---
+++ basics/function/funtionargument.py
@@ -0,0 +1,24 @@
+# Required arguments
+
+def agrfunc(a=2,b=3):
+    print("the average is: ",(a+b)/2);
+agrfunc(6,8)
+
+
+# Default arguments
+
+def agrfunc2(a,b, c=5):
+    print("the average is: ",(a+b+c)/2);
+    
+agrfunc2(6,7)
+    
+    
+# key argumetns  
+    
+def agrfunc3(a=2,b=3):
+    print("the average is: ",(a+b)/2);
+agrfunc3(b=6,a=8) 
+    
+    
+    
+    
