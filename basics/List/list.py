827d704bf32fa0d58c83a39843ece462e3287514
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-13 22:12:12 +0530

    list

---
+++ basics/List/list.py
@@ -0,0 +1,56 @@
+marks=[3,6,8,"Harry", True];
+
+# print(marks);
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# print(marks[3])
+# print(marks[4])
+# print(len(marks))
+# print(marks[-2])
+# print(marks[len(marks)-2])
+# print(marks[-2]==marks[len(marks)-2])
+
+
+# if "Harry" in marks:
+#     print("Yes")
+# else:
+#     print("No")
+
+
+# if 9 in marks:
+#     print("Yes")
+# else:
+#     print("No")
+    
+      
+# if "8" in marks:
+#     print("Yes")
+# else:
+#     print("No")    
+
+
+
+# if "Harr" in marks[3]:
+#     print("Yes")
+# else:
+#     print("No")
+
+
+
+# print(marks[1:-1])
+# print(marks[1:4:2])
+
+lst=[i*i for i in range(10)]
+print(lst);
+
+
+lst=[i*i for i in range(10) if i%2==0]
+print(lst);
+
+
+
+
+
+
