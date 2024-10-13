b2823dbfc367524fe495cb5c2c442110c3decc89
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-13 22:26:03 +0530

    list method

---
+++ basics/List/listMethod.py
@@ -0,0 +1,27 @@
+list=[11,45,1,2,6,4,45,6,45];
+
+list.append(10);
+
+# print(list);
+# list.sort();
+# print(list);
+# list.sort(reverse=True);
+# print(list);
+# print(list.index(1))
+# print(list.count(45))
+
+# m=list.copy();
+# print(m);
+
+# list.insert(1,899);
+# print(list)
+
+
+n=[900,600,200];
+# list.extend(n);
+# print(list);
+
+k=list+n; #concatenation
+print(k)
+
+
