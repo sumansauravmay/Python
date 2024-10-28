b5be7804faf63443a2c4d4769c4a4c057fe3ba67
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-28 16:05:08 +0530

    tupple

---
+++ basics/tupple/2.py
@@ -0,0 +1,37 @@
+#Update the tuple
+
+# countries=("Spain", "Italy", "India", "England");
+
+# temp=list(countries);
+# temp.append("Russia"); #add item
+# temp.pop(3); #remove item
+# temp[2]="Finland"; #change item
+# countries=tuple(temp);
+# print(countries);
+
+
+
+
+
+#concate the tuple
+# countries2=("Spain", "Italy", "India", "England");
+# countries3=("Switzerland", "USA", "Germany", "Japan");
+# allcountries=countries2+countries3;
+
+# print(allcountries);
+
+
+
+
+
+tuple1=(3,1,2,5,4,5,0,3,3,2,1,3);
+res=tuple1.count(3); #occurance
+res2=tuple1.count(10); #occurance
+res3=tuple1.index(5); #index
+res4=tuple1.index(3,4,8);# index of 3, between index 4 to 8
+print(res4);
+
+
+
+
+
