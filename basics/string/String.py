786e6826a1f9648b39fa2ad8545ef8dc4e843459
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-29 18:26:30 +0530

    fstring

---
+++ basics/string/String.py
@@ -0,0 +1,60 @@
+# z='I am "suman"'
+# print(z);
+
+
+# p='''Hi suman
+# I am from Delhi
+# I am software developer
+# '''
+# print(p);
+
+
+name="Suman";
+length=len(name);
+# print(name[0]);
+# print(name[1]);
+# print(name[2]);
+# print(name[3]);
+# print(name[4]);
+
+# for character in name:
+#     print(character);
+    
+    
+# print(name[0:3], length);    
+# print(name[0:length]);
+# print(name[1:length]);
+# print(name[:length]);
+# print(name[0:length-3]);
+# print(name[0:-3]);
+# print(name[-4:-2]);
+
+# print(name.upper());
+# print(name.lower());
+
+
+# print(name.replace("suman", "Akash"))
+# print(name.replace("Suman", "Akash"))
+# print(name.replace(name, "Akash"))
+
+
+
+# name2="!!!!!Suman!!!!!!!!";
+# print(name2.rstrip("!"));
+
+
+x="I am Suman Saurav";
+
+# print(x.split(" "));
+# print(x.lower().count("s"))
+# print(x.endswith("a"));
+# print(x.endswith("v"));
+print(x.find("am"));
+print(x.find("v"));
+print(x.index("I"))
+
+
+
+
+
+
---
+++ basics/string/fstring.py
@@ -0,0 +1,38 @@
+# letter="Hey my name is {} and I am {}";
+
+# country="India";
+# name="Suman";
+# x=letter.format(name, country)
+# y=letter.format(country, name)
+# print(y);
+
+
+
+
+
+# letter2="Hey my name is {1} and I am {0}";
+
+# country2="India";
+# name2="Suman";
+# z=letter2.format(country2, name2)
+# print(z);
+
+
+
+# country3="India";
+# name3="Suman";
+# print(f"Hey my name is {name3} and I am {country3}")
+
+
+
+
+price=30.9145453;
+txt=f"Price is only {price} dollars!";
+txt2=f"Price is only {"price"} dollars!";
+txt3=f"Price is only {price: .2f} dollars!";
+print(txt)
+print(txt2)
+print(txt3)
+
+
+
