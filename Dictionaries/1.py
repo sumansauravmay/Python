d7a2b0d7b5c351ace576ebb85395c92caa295e96
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-30 18:40:29 +0530

    dic

---
+++ Dictionaries/1.py
@@ -0,0 +1,23 @@
+dic={
+    "Harry":"Human being",
+    "Spoon":"Object",
+    1:"Suman",
+    2:"Saurav"
+}
+
+# print(dic["Harry"])
+# print(dic[2])
+# print(dic.get("Spoon"))
+# print(dic.get(2))
+# print(dic.keys())
+# print(dic.values())
+
+
+# for key in dic.keys():
+#     print(key, dic[key])
+
+
+# print(dic.items())
+
+for key, value in dic.items():
+    print(key, value)
