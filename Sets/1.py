3af52caa83f90bb98429f2aad890d61fd10c911b
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-30 06:38:25 +0530

    set definition

---
+++ Sets/1.py
@@ -0,0 +1,13 @@
+# set is defined as it is a well defined method
+# it doesn't maintain order
+
+s={12,1,2,3,2,8,9,3,6,7,9};
+info={"carlo", 19, False, 5.9, 19};
+set=set();
+print(s);
+print(type(set));
+print(info);
+
+for value in info:
+    print(value);
+
