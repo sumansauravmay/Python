9e7f6e0a64f1471e9b76853a2bc8c98d4ac3831f
sumansauravmay <sumansauravmay12345@gmail.com>
2024-11-04 15:31:34 +0530

    shorthand

---
+++ exercise/secretCodeLanguage.py
@@ -0,0 +1,16 @@
+#write a python program to translate a message into secret codelanguage. Use the rules below to translate normal English into secret code language.
+
+
+
+#coding
+# if the word contains atleast 3 characters, remove the first letter and append it at the end
+# now append three random characters at the starting and the end
+# else:
+#     simply reverse the string
+
+#Decoding
+# if the word contains less than 3 characters, reverse it 
+# else:
+#     remove 3 random characters from start and end, now remove the last letter 
+
+
