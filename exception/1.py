8c484b09af4973f78e3e3c0b076f25cb672f420e
sumansauravmay <sumansauravmay12345@gmail.com>
2024-11-04 13:20:19 +0530

    exception

---
+++ exception/1.py
@@ -0,0 +1,33 @@
+a=int(input("Enter number: "));
+
+# for i in range(1,11):
+#     print(f"Multiplication of {a} is  {a} X {i}={int(a)*i}")
+
+
+# print(f"Multiplication of {a} is")
+# for i in range(1,11):
+#     print(f"{a} X {i}={int(a)*i}")
+
+
+
+print(f"Multiplication of {a} is")
+try:
+   for i in range(1,11):
+    print(f"{a} X {i}={a*i}");
+except Exception as e:
+       print(e);
+       print("Invalid Input!");
+    
+
+print("Some imp lines of code!");
+print("End of program!");
+
+
+
+
+
+
+
+
+
+
