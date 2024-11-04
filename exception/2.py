64062413427009e39245c52775f1ff2fa011746f
sumansauravmay <sumansauravmay12345@gmail.com>
2024-11-04 13:26:39 +0530

    finally

---
+++ exception/2.py
@@ -0,0 +1,15 @@
+l=[1,5,6,7];
+i=input("Enter the intex: ");
+# print(l[i]);
+
+try:
+    for j in range(1,11):
+        print(f"{i}X{j} = {int(i)*j}");
+except:
+    print("Invalid input");
+finally:
+    print("I am always executed!");
+    
+    
+    
+    
