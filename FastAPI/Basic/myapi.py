3c1c098d8bac957d298a4d5c036da91281203835
sumansauravmay <sumansauravmay12345@gmail.com>
2025-01-05 23:35:13 +0530

    get api

--- FastAPI/Basic/myapi.py
+++ FastAPI/Basic/myapi.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI;
+from fastapi import FastAPI, Path;
 
 app=FastAPI();
 
@@ -16,8 +16,13 @@ def index():
     return {"name": "Welcome...."}
 
 
+# @app.get("/get-student/{student_id}")
+# def get_student(student_id:int):
+#     return students[student_id]
+
+
 @app.get("/get-student/{student_id}")
-def get_student(student_id:int):
+def get_student(student_id:int=Path(..., description="the ID of the student we want to view", gt=0, lt=3)):
     return students[student_id]
 
 
